e8cdb5df73a8b7cef841f95b5477418a0eb48098
lucas solari <solarilucas92@gmail.com>
2023-12-13 10:01:03 +0100

    m

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import styles from "./Portfolio.module.css";
 const Portfolio = () => {
     return(
         <div className={styles.portfolio_container}>
-
+            <h2 className={styles.portfolio_title}><strong>Some of my works</strong></h2>
         </div>
     )
 }
