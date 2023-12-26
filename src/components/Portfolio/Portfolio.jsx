98d24e6f00fe366e837f535f405eff513ed3fb40
lucas solari <solarilucas92@gmail.com>
2023-12-26 12:51:26 +0100

    m

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,22 @@
 import styles from "./Portfolio.module.css";
 
-const Portfolio = () => {
-    return(
-        <div className={styles.portfolio_container}>
-            <h2 id="portfolio" className={styles.portfolio_title}><strong>Some of my works</strong></h2>
-            <div className={styles.works_logo_container}>
+import img1 from "../../assets/star-wars.png"
 
+const Portfolio = () => {
+    return (
+        <div className={styles.portfolio_container}>
+            <h2 id="portfolio" className={styles.portfolio_title}><strong>Some of my work</strong></h2>
+            <div className={styles.works_logo_container}>
+                <a href="https://star-wars-reloaded.vercel.app/" target="_blank" className={styles.anchor}><div className={styles.works_logo}>
+                    <div className={styles.starwars_container}>
+                        <img src={img1} className={styles.img_logo} alt="star-wars" />
+                    </div>
+                    <p className={styles.works_texts}><strong>FrontEnd project, connected with external API. Login with JWT Tokens</strong></p>
+                </div>
+                </a>
+                <div className={styles.works_logo}></div>
+                <div className={styles.works_logo}></div>
+                <div className={styles.works_logo}></div>
             </div>
         </div>
     )
