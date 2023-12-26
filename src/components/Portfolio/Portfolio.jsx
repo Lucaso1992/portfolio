de175da14ad876701aff89f8cb3347cee9b1aae1
lucas solari <solarilucas92@gmail.com>
2023-12-26 14:06:20 +0100

    m

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,21 +1,35 @@
 import styles from "./Portfolio.module.css";
 
 import img1 from "../../assets/star-wars.png"
+import img2 from "../../assets/we-contain.png"
+import img3 from "../../assets/cosmos.png"
 
 const Portfolio = () => {
     return (
         <div className={styles.portfolio_container}>
             <h2 id="portfolio" className={styles.portfolio_title}><strong>Some of my work</strong></h2>
             <div className={styles.works_logo_container}>
+                <a href="https://www.wecontain.com.ar/" target="_blank" className={styles.anchor}><div className={styles.works_logo}>
+                    <div className={styles.wecontain_container}>
+                        <img src={img2} className={styles.img_logo} alt="we-contain" />
+                    </div>
+                    <p className={styles.works_texts}><strong>Design and development of a corporate website for a container rental company.</strong></p>
+                </div>
+                </a>
+                <a href="https://cosmos-app-1234.onrender.com/" target="_blank" className={styles.anchor}><div className={styles.works_logo}>
+                    <div className={styles.cosmos_container}>
+                        <img src={img3} className={styles.img_logo} alt="we-contain" />
+                    </div>
+                    <p className={styles.works_texts}><strong> FrontEnd Development of a Dating Web App connecting people through astrology.</strong></p>
+                </div>
+                </a>
                 <a href="https://star-wars-reloaded.vercel.app/" target="_blank" className={styles.anchor}><div className={styles.works_logo}>
                     <div className={styles.starwars_container}>
                         <img src={img1} className={styles.img_logo} alt="star-wars" />
                     </div>
-                    <p className={styles.works_texts}><strong>FrontEnd project, connected with external API. Login with JWT Tokens</strong></p>
+                    <p className={styles.works_texts}><strong>FrontEnd development, connected with external API. Login with JWT Tokens.</strong></p>
                 </div>
                 </a>
-                <div className={styles.works_logo}></div>
-                <div className={styles.works_logo}></div>
                 <div className={styles.works_logo}></div>
             </div>
         </div>
