46276d687931375d180da3d484f8edd07040338c
lucas solari <solarilucas92@gmail.com>
2023-12-27 17:26:14 +0100

    m

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,55 +3,42 @@ import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+
 
   return (
     <nav className={`${styles.navbar} navbar navbar-expand-lg`}>
-      <div className="container-fluid">
-        <button
-          className="navbar-dark navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon ms-auto"></span>
+    <div className="container-fluid">
+        <button className="navbar-dark navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className=" navbar-toggler-icon"></span>
         </button>
-        <div
-          className={`${styles.navCollapse} collapse navbar-collapse ${isOpen ? "show" : ""}`}
-          id="navbarNav"
-        >
-          <ul className="navbar-nav ms-auto">
-            <a
-              className={`${styles.nav_items_one} nav-link`}
-              href="#"
-            >
-              <strong className={styles.strong_items}>Home</strong>
-            </a>
-            <a
-              className={`${styles.nav_items} nav-link`}
-              href="#portfolio"
-            >
-              <strong className={styles.strong_items}>Portfolio</strong>
-            </a>
-            <a
-              className={`${styles.nav_items} nav-link`}
-              href="#"
-            >
-              <strong className={styles.strong_items}>About me</strong>
-            </a>
-            <a
-              className={`${styles.nav_items} nav-link me-4`}
-              href="#"
-            >
-              <strong className={styles.strong_items}>Contact</strong>
-            </a>
-          </ul>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    <a className={`${styles.nav_items_one} nav-link`} href="#">
+                    <strong className={styles.strong_items}>Home</strong>
+                    </a>
+                </li>
+                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    <a className={`${styles.nav_items} nav-link`} href="#portfolio">
+                    <strong className={styles.strong_items}>Portfolio</strong>
+                    </a>
+                </li>
+                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    <a className={`${styles.nav_items} nav-link`} href="#">
+                    <strong className={styles.strong_items}>About me</strong>
+                    </a>
+                </li>
+                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                    <a className={`${styles.nav_items} nav-link me-4`} href="#">
+                    <strong className={styles.strong_items}>Contact</strong>   
+                    </a>
+                </li>
+            </ul>
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
   );
 };
 
