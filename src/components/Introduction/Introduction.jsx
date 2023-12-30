13b8620eb1333282b9852672cd3de71aa231cb8d
lucas solari <solarilucas92@gmail.com>
2023-12-30 19:27:18 +0100

    m

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,19 +1,55 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import styles from "./Introduction.module.css";
 
 import img from "../../assets/profile-img.jpg"
 
 const Introduction = () => {
-
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
     const targetRef = useRef < HTMLDivElement > (null);
-    const { scrollYProgress } = useScroll({
-        target: targetRef,
-        offset: ["start start", "end end"]
-    });
 
-    const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
+    const useMediaQuery = (query) => {
+        return new Promise((resolve) => {
+          const mediaQuery = window.matchMedia(query);
+          resolve(mediaQuery.matches);
+          const handleMediaQueryChange = () => {
+            resolve(mediaQuery.matches);
+          };
+          mediaQuery.addEventListener("change", handleMediaQueryChange);
+          return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+          };
+        });
+      };
+
+      useEffect(() => {
+        const fetchMediaQuery = async () => {
+          const isSmall = await useMediaQuery("(max-width: 767px)");
+          setIsSmallScreen(isSmall);
+        };
+    
+        fetchMediaQuery();
+      }, []);
+
+      const { scrollYProgress: scrollYProgressDesktop } = useScroll({
+        target: targetRef,
+        offset: ["start start", "end end"],
+      });
+
+      const { scrollYProgress: scrollYProgressMobile } = useScroll({
+        target: targetRef,
+        offset: ["start start", "end end"], 
+      });
+
+      const opacityDesktop = useTransform(scrollYProgressDesktop, [0, 0.5], [1, 0]);
+      const opacityMobile = useTransform(scrollYProgressMobile, [0, 0.1], [1, 0], {
+        clamp: false, 
+      });
+    
+      const opacity = isSmallScreen ? opacityMobile : opacityDesktop;
+
+
     return (
 
         <motion.section style={{ opacity }} ref={{ targetRef }} className={styles.intro_container}>
