82cbb1e7620cc9087bd3e3f7e93fd23b0702458c
lucas solari <solarilucas92@gmail.com>
2024-01-08 16:33:14 +0100

    m

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -7,37 +7,39 @@ import styles from "./Introduction.module.css";
 import img from "../../assets/profile-img.jpg"
 
 const Introduction = () => {
-      const { store } = useAppContext();
-      const targetRef = useRef<HTMLDivElement>(null);
+  const { store } = useAppContext();
+  const targetRef = useRef < HTMLDivElement > (null);
 
-      const { scrollYProgress } = useScroll({
-        target: targetRef,
-        offset: ["start start", "end start"],
-      });
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start start", "end start"],
+  });
 
-      const opacityDesktop = useTransform(scrollYProgress, [0, 0.12], [1, 0]);
-      const opacityMobile = useTransform(scrollYProgress, [0, 0.14], [1, 0], {
-        clamp: false, 
-      });
-    
-      const opacity = store.isSmallScreen ? opacityMobile : opacityDesktop;
+  const opacityDesktop = useTransform(scrollYProgress, [0, 0.12], [1, 0]);
+  const opacityMobile = useTransform(scrollYProgress, [0, 0.14], [1, 0], {
+    clamp: false,
+  });
+
+  const opacity = store.isSmallScreen ? opacityMobile : opacityDesktop;
 
 
-    return (
+  return (
 
-        <motion.section style={{ opacity }} ref={{ targetRef }} className={styles.intro_container}>
-            <div className={styles.intro_text_container}>
-                <h2 className={styles.welcome_message}><mark className={styles.remarked}><strong>Hi there! Welcome</strong></mark></h2>
-                <p className={styles.intro_textito}>My name is <strong>Lucas Solari</strong>, I´m a Front-End Developer. Design and usability
-                    enthusiast, I enjoy crafting high-quality web solutions and delivering outstanding user experiences.
-                    Have a look at my works and if you have any querry, feel free to reach out!</p>
-            </div>
-            <div className={styles.img_container}>
-                <img className={`${styles.imgg}  object-fit-cover w-100 h-100`} src={img} alt="" />
-            </div>
-        </motion.section>
+    <motion.section style={{ opacity }} ref={{ targetRef }} className={styles.intro_div}>
+      <div className={styles.intro_container}>
+        <div className={styles.texts_container}>
+        <h2 className={styles.welcome_message}><mark className={styles.remarked}><strong>Hi there! Welcome</strong></mark></h2>
+        <p className={styles.intro_textito}>My name is <strong>Lucas Solari</strong>, I´m a Front-End Developer. Design and usability
+          enthusiast, I enjoy crafting high-quality web solutions and delivering outstanding user experiences.
+          Have a look at my works and if you have any querry, feel free to reach out!</p>
+          </div>
+        <div className={styles.img_container}>
+          <img className={`${styles.imgg}  object-fit-cover w-100 h-100`} src={img} alt="" />
+        </div>
+      </div>
+    </motion.section>
 
-    )
+  )
 }
 
 export default Introduction;
