0df4e0db3229afc96238fcabe758b10ff0f149f4
lucas solari <solarilucas92@gmail.com>
2024-01-10 12:20:46 +0100

    m

--- src/components/About me/About-me.jsx
+++ src/components/About me/About-me.jsx
@@ -35,7 +35,7 @@ const AboutMe = () => {
     <motion.section id="about" style={{ opacity }} ref={{ targetRef }} className={styles.about_div}>
       <div className={styles.about_container}>
         <div className={styles.first_div}>
-          <h2 className={styles.about_titles}><strong> About me</strong></h2>
+          <h2 className={styles.about_titles}><strong>About me</strong></h2>
           <p className={styles.aboutme_intro}>Passionate Front-End Developer. Raised in a generation surrounded by the expansion of the Internet, I became curious about coding, so I started my research with tutorials and the vast amount of free materials available on the web. In 2023, I made the decision to formalize my studies. Always striving to deliver more than expected, I appreciate the world of development and coding for its challenges and continuous growth. Take a look at my skills.</p>
           <h2 className="mt-2"><strong>Education</strong></h2>
           <div className="d-flex flex-row align-items-center mt-2">
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,14 @@
 import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { Link } from "react-router-dom";
 
 import useAppContext from "../../store/AppContext";
+import { IoPerson } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 import styles from "./Contact.module.css";
 
@@ -32,7 +38,7 @@ const Contact = () => {
       .then((result) => {
         console.log(result.text);
         form.current.reset();
-        alert("Message sent! ;)");
+        alert("Message sent correctly! thanks ;)");
       }, (error) => {
         console.log(error.text);
       });
@@ -40,22 +46,21 @@ const Contact = () => {
 
   return (
     <motion.section id="contact" style={{ opacity }} ref={{ targetRef }} className={styles.contact_div}>
-      <h2 className={styles.contact_title}> <strong>Contact</strong></h2>
+      <h2 className={styles.contact_title}><strong>Contact</strong></h2>
       <div className={styles.contact_container}>
         <div className={styles.my_info}>
-          <p>Lucas Solari</p>
-          <p> 617 2728 72</p>
-          <p>solarilucas92@gmail.com</p>
+          <p className={styles.contact_myname}><strong>Lucas Solari</strong></p>
+          <Link to={"https://api.whatsapp.com/send?phone=34617272872"} className={`${styles.links_div} text-decoration-none`}><div className={styles.icons_conteiner}><FaPhone className={styles.icons} /><p className={styles.icons_text}>(+34) 617 272 872</p></div></Link>
+          <Link to={"mailto:solarilucas92@gmail.com"} className={`${styles.links_div} text-decoration-none`}><div className={styles.icons_conteiner}><MdEmail className={styles.icons} /><p className={styles.icons_text}>solarilucas92@gmail.com</p></div></Link>
+          <Link to={"https://github.com/Lucaso1992"} target="_blank" className={`${styles.links_div} text-decoration-none`}><div className={styles.icons_conteiner}><FaGithub className={styles.icons} /><p className={styles.icons_text}>github.com/Lucaso1992</p></div></Link>
+          <Link to={"https://www.linkedin.com/in/lucas-solari/"} target="_blank" className={`${styles.links_div} text-decoration-none`}><div className={styles.icons_conteiner}><FaLinkedin className={styles.icons} /><p className={styles.icons_text}>linkedin.com/in/lucas-solari</p></div></Link>
         </div>
-      <form ref={form} onSubmit={sendEmail} className={styles.form}>
-        {/* <label>Name</label> */}
-        <input type="text" name="user_name" required placeholder="Name"/>
-        {/* <label>Email</label> */}
-        <input type="email" name="user_email" required placeholder="Email"/>
-        {/* <label>Message</label> */}
-        <textarea name="message" required placeholder="Message"/>
-        <input type="submit" value="Send" />
-      </form>
+        <form ref={form} onSubmit={sendEmail} className={styles.form}>
+          <input type="text" name="user_name" className={styles.input_holder} required placeholder="Name" />
+          <input type="email" name="user_email" className={styles.input_holder} required placeholder="Email" />
+          <textarea name="message" className={styles.message_holder} required placeholder="Message" />
+          <input type="submit" className={styles.send_button} value="Send" />
+        </form>
       </div>
     </motion.section>
   )
