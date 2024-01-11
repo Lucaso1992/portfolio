7d8a6723daeb6233329e28cbded4887d674d043d
lucas solari <solarilucas92@gmail.com>
2024-01-11 19:26:55 +0100

    m

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -31,7 +31,7 @@ const Introduction = () => {
         <h2 className={styles.welcome_message}><mark className={styles.remarked}><strong>Hi there! Welcome</strong></mark></h2>
         <p className={styles.intro_textito}>My name is <strong>Lucas Solari</strong>, I´m a Front-End Developer. Design and usability
           enthusiast, I enjoy crafting high-quality web solutions and delivering outstanding user experiences.
-          Have a look at my works and if you have any querry, feel free to reach out!</p>
+          Have a look at my works and if you have any query, feel free to reach out!</p>
           </div>
         <div className={styles.img_container}>
           <img className={`${styles.imgg}  object-fit-cover w-100 h-100`} src={img} alt="" />
