4793d2e268a58cc1ae79627ec3b68e614e1d8ccf
lucas solari <solarilucas92@gmail.com>
2024-09-19 12:33:47 +0200

    cv

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,42 +5,43 @@ import { useTranslation } from 'react-i18next';
 const Navbar = () => {
     const [t] = useTranslation('global');
 
-  return (
-    <nav className={`${styles.navbar} navbar navbar-expand-lg`}>
-    <div className="container-fluid">
-        <TraductorButton/>
-        <button className={`${styles.toggler} navbar-dark navbar-toggler`} type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className=" navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                    <a className={`${styles.nav_items_one} nav-link`} href="#">
-                    <strong className={styles.strong_items}>{t('navbar.one')}</strong>
-                    </a>
-                </li>
-                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                    <a className={`${styles.nav_items} nav-link`} href="#portfolio">
-                    <strong className={styles.strong_items}>{t('navbar.two')}</strong>
-                    </a>
-                </li>
-                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                    <a className={`${styles.nav_items} nav-link`} href="#about">
-                    <strong className={styles.strong_items}>{t('navbar.three')}</strong>
-                    </a>
-                </li>
-                <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                    <a className={`${styles.nav_items} nav-link me-4`} href="#contact">
-                    <strong className={styles.strong_items}>{t('navbar.four')}</strong>   
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-  );
+    return (
+        <nav className={`${styles.navbar} navbar navbar-expand-lg`}>
+            <div className="container-fluid">
+                <TraductorButton />
+                <button className={`${styles.toggler} navbar-dark navbar-toggler`} type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className=" navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                            <a className={`${styles.nav_items_one} nav-link`} href="#">
+                                <strong className={styles.strong_items}>{t('navbar.one')}</strong>
+                            </a>
+                        </li>
+                        <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                            <a className={`${styles.nav_items} nav-link`} href="#portfolio">
+                                <strong className={styles.strong_items}>{t('navbar.two')}</strong>
+                            </a>
+                        </li>
+                        <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                            <a className={`${styles.nav_items} nav-link`} href="#about">
+                                <strong className={styles.strong_items}>{t('navbar.three')}</strong>
+                            </a>
+                        </li>
+                        <li className="nav-item" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                            <a className={`${styles.nav_items} nav-link me-4`} href="#contact">
+                                <strong className={styles.strong_items}>{t('navbar.four')}</strong>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
